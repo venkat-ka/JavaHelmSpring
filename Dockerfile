ce55cd497753af8f28e75de082ce5d063071fe72
venkat-ka <venkat.k2516@gmail.com>
2026-02-08 19:14:21 +0530

    add github cicd pipeline

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM eclipse-temurin:17-jre
-WORKDIR /app
-COPY target/demo-0.0.1.jar app.jar
-ENTRYPOINT ["java","-jar","/app/app.jar"]
---
+++ app/src/main/java/com/example/javahelmspring/JavaHelmSpringApplication.java
@@ -0,0 +1,16 @@
+package com.example.javahelmspring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class JavaHelmSpringApplication {
+ public static void main(String[] args){
+   SpringApplication.run(JavaHelmSpringApplication.class,args);
+ }
+ @GetMapping("/")
+ public String home(){
+   return "JavaHelmSpring running on EKS";
+ }
+}
